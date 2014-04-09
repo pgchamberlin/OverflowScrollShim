4bb4786feb4ed97b62996beaaf711eb42830a013
Peter Chamberlin <pgchamberlin@debian>
2014-04-09 17:08:09 +0000

    Added shim and demo page

---
+++ overflow-scroll-shim.js
@@ -0,0 +1,31 @@
+(function() {
+    var touchScroll = function() {
+        var elements = document.getElementsByClassName('responsive-table-container'),
+        scrollStartPosX = 0,
+        i = 0;
+        for (i; i < elements.length; i++) {
+            elements[i].addEventListener("touchstart", function(event) {
+                var target = event.currentTarget;
+                // accesses then explicitly sets the container div's width
+                target.style.width = target.style.width;
+                scrollStartPosX = target.scrollLeft + event.touches[0].pageX;
+            }, false);
+
+            elements[i].addEventListener("touchmove", function(event) {
+                var target = event.currentTarget;
+                if ((target.scrollLeft < target.scrollWidth - target.offsetWidth && target.scrollLeft + event.touches[0].pageX < scrollStartPosX - 5) || (target.scrollLeft != 0 && target.scrollLeft + event.touches[0].pageX > scrollStartPosX + 5)) {
+                    event.preventDefault();
+                }
+                target.scrollLeft = scrollStartPosX - event.touches[0].pageX;
+            }, false);
+        }
+    },
+
+    isAndroid2xDevice = function() {
+        return navigator.userAgent.match(/Android 2/i) ? true : false;
+    };
+
+    if (isAndroid2xDevice()) {
+        touchScroll();
+    }
+})();
