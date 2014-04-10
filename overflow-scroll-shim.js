6833a73d8bbfd12cef0c00b116347e15cd53e894
Integralist <mark.mcdx@gmail.com>
2014-04-10 09:04:22 +0100

    Convert anonymous functions into function declarations to make code easier to read

--- overflow-scroll-shim.js
+++ overflow-scroll-shim.js
@@ -4,19 +4,22 @@
         scrollStartPosX = 0,
         i = 0;
         for (i; i < elements.length; i++) {
-            elements[i].addEventListener("touchstart", function(event) {
-                var target = event.currentTarget;
-                scrollStartPosX = target.scrollLeft + event.touches[0].pageX;
-            }, false);
-
-            elements[i].addEventListener("touchmove", function(event) {
-                var target = event.currentTarget;
-                if ((target.scrollLeft < target.scrollWidth - target.offsetWidth && target.scrollLeft + event.touches[0].pageX < scrollStartPosX - 5) || (target.scrollLeft != 0 && target.scrollLeft + event.touches[0].pageX > scrollStartPosX + 5)) {
-                    event.preventDefault();
-                }
-                target.scrollLeft = scrollStartPosX - event.touches[0].pageX;
-            }, false);
+            elements[i].addEventListener("touchstart", handleTouchStart, false);
+            elements[i].addEventListener("touchmove", handleTouchMove, false);
         }
+    }
+
+    function handleTouchStart (event) {
+        var target = event.currentTarget;
+        scrollStartPosX = target.scrollLeft + event.touches[0].pageX;
+    }
+
+    function handleTouchMove (event) {
+        var target = event.currentTarget;
+        if ((target.scrollLeft < target.scrollWidth - target.offsetWidth && target.scrollLeft + event.touches[0].pageX < scrollStartPosX - 5) || (target.scrollLeft != 0 && target.scrollLeft + event.touches[0].pageX > scrollStartPosX + 5)) {
+            event.preventDefault();
+        }
+        target.scrollLeft = scrollStartPosX - event.touches[0].pageX;
     }
 
     function isAndroid2xDevice() {
